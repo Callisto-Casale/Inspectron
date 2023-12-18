c8ac17224ed01e2ef0ed46512f902499d354af4d
Callisto <olav.kuhnen1@hotmail.com>
2023-12-18 12:00:28 +0100

    Added option to apply autopep8 before applying PyLint to files

--- main.py
+++ main.py
@@ -6,16 +6,26 @@ import random
 import datetime
 from bs4 import BeautifulSoup
 
-def main(folder):
+
+def main(folder, autopep8=False):
     os.system("cls")
+
+    if (autopep8):
+        print("[INSPECTRON] Running autopep8 on all files in " + folder)
+        for file in os.listdir(folder):
+            if file.endswith(".py"):
+                cmd = "autopep8 --in-place --aggressive --aggressive " + folder + "/" + file
+                process = subprocess.run(cmd, shell=True, text=True)
+
     print("[INSPECTRON] Running pylint on all files in " + folder)
     for file in os.listdir(folder):
         if file.endswith(".py"):
             cmd = "pylint " + folder + "/" + file
-            process = subprocess.run(cmd, shell=True, text=True, capture_output=True)
-            output = process.stdout.split("\n")[-3].split("(")[0].split("at ")[-1]
+            process = subprocess.run(
+                cmd, shell=True, text=True, capture_output=True)
+            output = process.stdout.split(
+                "\n")[-3].split("(")[0].split("at ")[-1]
             print(f"[{file}] - {output}")
-    
 
     path = "outputs"
     filename = datetime.datetime.now().strftime("%d-%m-%Y-%H-%M-%S") + ".html"
@@ -26,15 +36,13 @@ def main(folder):
     with open("outputs/template.html", 'r') as fp:
         soup = BeautifulSoup(fp, 'html.parser')
 
-    # add the output of pylint to the html file but keep new lines
-          
-    tbody = soup.find(id="report-output")  
-    # ...
+    tbody = soup.find(id="report-output")
 
     files = [file for file in os.listdir(folder) if file.endswith(".py")]
     for i, file in enumerate(files):
         cmd = "pylint " + folder + "/" + file
-        process = subprocess.run(cmd, shell=True, text=True, capture_output=True)
+        process = subprocess.run(
+            cmd, shell=True, text=True, capture_output=True)
         output = process.stdout.split("\n")
         for j, line in enumerate(output):
             new_row = soup.new_tag("tr")
@@ -43,31 +51,31 @@ def main(folder):
             new_cell_output = soup.new_tag("td")
             new_cell_output.string = line
             new_cell_checkbox = soup.new_tag("td")
-            new_checkbox = soup.new_tag("input", type="checkbox", onchange="strikeThrough(this)", class_="styled-checkbox")
+            new_checkbox = soup.new_tag(
+                "input",
+                type="checkbox",
+                onchange="strikeThrough(this)",
+                class_="styled-checkbox")
             new_cell_checkbox.append(new_checkbox)
             new_row.append(new_cell_num)
             new_row.append(new_cell_output)
             new_row.append(new_cell_checkbox)
             tbody.append(new_row)
-        if i < len(files) - 1:  # don't add gap after the last output
-            gap = soup.new_tag("tr", style="height: 50px;")  # adjust the height as needed
+        if i < len(files) - 1:
+            gap = soup.new_tag("tr", style="height: 50px;")
             tbody.append(gap)
-
-    # ...
 
     with open(fullPath, 'w') as fp:
         fp.write(str(soup))
-
-    # ...
 
     print(f"[INSPECTRON] Opening optimization report in browser")
     os.system(os.getcwd() + "/" + fullPath)
 
 
-
-
-
-
 if __name__ == "__main__":
     folder = sys.argv[1]
+    if ("-ap8" in sys.argv):
+        main(folder, autopep8=True)
+
+    print(sys.argv)
     main(folder)
